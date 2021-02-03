5f47e300220b6281dbf63e14bfcec306a5bdaccf
brian202 <42294847+brian202@users.noreply.github.com>
2021-02-03 14:00:51 +0900

    Initial commit

---
+++ src/views/nested/menu1/menu1-2/menu1-2-2/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="padding:30px;">
+    <el-alert
+      :closable="false"
+      title="메뉴 1-2-2"
+      type="warning"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Menu1-2-2'
+})
+export default class extends Vue {}
+</script>
